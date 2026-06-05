a3dc57e386562c7e5f24ab8fd271bd94091114db
Shai Cioara <shai.cioara@priority-software.com>
2026-06-05 22:21:41 +0300

    cleaned up plugins

--- init.lua
+++ init.lua
@@ -1,73 +1,100 @@
--- Set leader keys before anything else
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
--- Load core config
-require("config.options")
+local opt = vim.opt
+local fd = function(pat)
+    local fdout = vim.system({
+        "fd",
+        "--type", "f",
+        "--hidden",
+        "--follow",
+        "--full-path",
+        "--exclude", ".git",
+        pat,
+    }):wait()
 
--- Clear search highlight
+    if fdout.code ~= 0 then
+        return {}
+    end
+
+    local matches = vim.split(fdout.stdout, "\n", { trimempty = true })
+    return matches
+end
+
+_G.fd_findfunc = function(cmdarg, cmdcomplete)
+    if not cmdcomplete then
+        local stat = vim.uv.fs_stat(cmdarg)
+        if stat and stat.type == 'file' then
+            return { cmdarg }
+        end
+        local matches = fd(cmdarg)
+        if #matches == 0 then
+            return {}
+        end
+        return { matches[1] }
+    end
+
+    return fd(cmdarg)
+end
+
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.hlsearch = true
+opt.incsearch = true
+opt.signcolumn = "yes"
+opt.cursorline = true
+opt.wrap = false
+opt.splitbelow = true
+opt.splitright = true
+opt.autoread = true
+opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+opt.foldmethod = "expr"
+opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+opt.foldlevelstart = 99
+opt.clipboard = "unnamedplus"
+opt.mouse = "a"
+opt.findfunc = "v:lua.fd_findfunc"
+opt.grepprg = "rg --vimgrep --smart-case --hidden --no-heading"
+opt.wildmode = "noselect:lastused,full"
+opt.wildoptions = "pum,tagfile,fuzzy"
+opt.completeopt = "fuzzy,menuone,noselect,popup,preview"
+
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
--- Toggle RTL for Hebrew
 vim.keymap.set("n", "<leader>rl", function()
     vim.wo.rightleft = not vim.wo.rightleft
 end, { desc = "Toggle RTL" })
+vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format({ async = true }) end, { desc = "Format" })
 
 vim.cmd.packadd('nvim.difftool')
 vim.cmd.packadd('nvim.undotree')
+
 local function gh(name)
     return "https://github.com/" .. name
 end
 
 vim.pack.add({
     gh("neovim/nvim-lspconfig"),
-    gh("echasnovski/mini.icons"),
-    gh("nvim-lualine/lualine.nvim"),
-    gh("ibhagwan/fzf-lua"),
     gh("lewis6991/gitsigns.nvim"),
-    gh('folke/lazydev.nvim'),
     gh('seblj/roslyn.nvim'),
     gh('nvim-treesitter/nvim-treesitter'),
-    gh('folke/which-key.nvim')
 })
-require('mini.icons').setup({})
-require('lazydev').setup({
-    opts = {
-        library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-        }
-    }
-})
-require('lualine').setup({
-    options = {
-        globalstatus = true,
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-    },
-    sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { { "filename", path = 1 } },
-        lualine_x = { "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-    }
-})
-require('which-key').setup({
-    preset = "helix",
-    spec = {
-        { "<leader>f", group = "Find" },
-        { "<leader>g", group = "Git" },
-        { "<leader>l", group = "LSP" },
-        { "<leader>t", group = "Terminal" },
-        { "<leader>u", group = "UI" },
-    },
-})
-require('plugins.lsp')
-require('plugins.fzf-lua')
-require('plugins.gitsigns')
-require('plugins.roslyn')
 
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("angularls")
+vim.lsp.enable("html")
+vim.lsp.enable("cssls")
+vim.lsp.enable("fsautocomplete")
+vim.lsp.enable("marksman")
+vim.lsp.enable("clangd")
 vim.cmd.colorscheme('habamax')
 
 vim.g.netrw_preview = 1
@@ -78,5 +105,3 @@ vim.g.netrw_banner = 0
 vim.g.netrw_list_hide = "netrw_gitignore#Hide()"
 vim.g.netrw_keepdir = 0
 vim.g.netrw_localcopydircmd = 'cp -r'
-vim.keymap.set("n", "<leader>e", ":Lexplore %:p:h<CR>", { silent = true })
-vim.keymap.set("n", "<leader>E", ":Lexplore<CR>", { silent = true })
